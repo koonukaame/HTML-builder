11ee3e4e85d13c4b07ef029def89e6b892047cd7
koonukaame <marialezhebokova@yandex.ru>
2025-01-17 20:48:24 +0300

    feat: solve html-builder task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path').join(__dirname, 'text.txt');
+
+const readline = require('readline');
+const stream = fs.createWriteStream(path, 'utf-8');
+
+const rlInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+}); // readline создает модуль для обработки потока ввода и вывода в консоль
+// input: process.stdin - поток ввода
+// input: process.stdout - поток вывода
+
+console.log(
+  'Hello! Write something in console. To exit type "exit" or press CTRL+C',
+);
+
+rlInterface.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    rlInterface.close();
+  }
+  stream.write(`${input}\n`);
+  console.log(`"${input}" is a pretty wise thing to say`);
+}); // line срабатывает если пользователь вводит в консоль и нажимает Enter
+
+rlInterface.on('close', () => {
+  console.log('Goodbye!');
+  stream.end();
+}); // close срабатывает если закрылся интерфейс ввода-вывода (при нажатии ctrl+c или при явном вызове rlInterface.close())
