1da021000fdd9e421e1930867c620f055400e75d
koonukaame <marialezhebokova@yandex.ru>
2025-01-19 15:29:57 +0300

    fix: fix copy-files directory  not updating its content after deleting files in files directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,6 +6,16 @@ const dirPathCopy = path.join(__dirname, 'files-copy');
 async function copyDir() {
   await fs.mkdir(dirPathCopy, { recursive: true });
   const files = await fs.readdir(dirPath, { withFileTypes: true });
+  const filesCopy = await fs.readdir(dirPathCopy, { withFileTypes: true });
+
+  await Promise.all(
+    filesCopy.map(async (fileCopy) => {
+      if (files.some((file) => fileCopy.name !== file.name)) {
+        const deletedFile = path.join(dirPathCopy, fileCopy.name);
+        fs.unlink(deletedFile);
+      }
+    }),
+  );
 
   await Promise.all(
     files.map(async (file) => {
